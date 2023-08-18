af25012780109f5738a34f7fa9ca89cd6d6f6dd2
Marchinka90 <martin_marchev@abv.bg>
2023-08-18 18:29:01 +0300

    Added new page for user places

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useParams } from 'react-router-dom'
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Bulding',
+        description: 'One of the most famous scy scrapers in the world!',
+        imageURL: 'https://lh3.googleusercontent.com/p/AF1QipOBBZgukGcL1DRhiwcJhYG_2o-1pPv5Wvs2M29B=s680-w680-h510',
+        address: '20 W 34th St., New York, NY 10001',
+        creator: 'u1',
+        location: {
+            lat: 40.7484405,
+            log: -73.9878584
+        },
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Bulding',
+        description: 'One of the most famous scy scrapers in the world!',
+        imageURL: 'https://lh3.googleusercontent.com/p/AF1QipOBBZgukGcL1DRhiwcJhYG_2o-1pPv5Wvs2M29B=s680-w680-h510',
+        address: '20 W 34th St., New York, NY 10001',
+        creator: 'u2',
+        location: {
+            lat: 40.7484405,
+            log: -73.9878584
+        },
+    }
+]
+
+const UserPlaces = props => {
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces}/>;
+};
+
+export default UserPlaces;
