57a27d766c80af56a3a80f3d772358d334a7aea3
Marchinka90 <martin_marchev@abv.bg>
2023-08-21 17:17:10 +0300

    Update controllers name

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const placesControllers = require('../controllers/places-controllers');
+const placesController = require('../controllers/places-controller');
 
-router.get('/:pid', placesControllers.getPlaceById);
+router.get('/:pid', placesController.getPlaceById);
 
-router.get('/user/:uid', placesControllers.getPlaceByUserId);
+router.get('/user/:uid', placesController.getPlaceByUserId);
 
 module.exports = router;
