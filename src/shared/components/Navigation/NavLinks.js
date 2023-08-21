d86d76487c68777b62783b09ff98bb080af36782
Marchinka90 <martin_marchev@abv.bg>
2023-08-21 12:27:24 +0300

    Added auth context for app-wide state management

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
+    const authCtx = useContext(AuthContext);
+
     return (
         <ul className='nav-links'>
             <li>
                 <NavLink to='/' exact>ALL USERS</NavLink>
             </li>
-            <li>
-                <NavLink to='/u1/places'>MY PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to='/places/new'>ADD PLACES</NavLink>
-            </li>
-            <li>
-                <NavLink to='/auth'>AUTHENTICATE</NavLink>
-            </li>
+            {authCtx.isLoggedIn && (
+                <li>
+                    <NavLink to='/u1/places'>MY PLACES</NavLink>
+                </li>
+            )}
+            {authCtx.isLoggedIn && (
+                <li>
+                    <NavLink to='/places/new'>ADD PLACES</NavLink>
+                </li>
+            )}
+            {!authCtx.isLoggedIn && (
+                <li>
+                    <NavLink to='/auth'>AUTHENTICATE</NavLink>
+                </li>
+            )}
+            {authCtx.isLoggedIn && (
+                <li>
+                    <button onClick={authCtx.logout}>LOGOUT</button>
+                </li>
+            )}
         </ul>
     );
 }
