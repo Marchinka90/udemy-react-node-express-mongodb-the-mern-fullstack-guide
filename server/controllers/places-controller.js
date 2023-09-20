5f21bd51338521e07fc09c67d923e0059d5bbe0c
Marchinka90 <martin_marchev@abv.bg>
2023-09-20 18:44:30 +0300

    Added new auth hook

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -48,7 +48,7 @@ const createPlace = async (req, res, next) => {
         return next(new HttpError('Invalid inputs passed, please check your data.', 422));
     }
 
-    const { title, description, address, creator } = req.body;
+    const { title, description, address } = req.body;
 
     let coordinates;
     try {
@@ -62,13 +62,13 @@ const createPlace = async (req, res, next) => {
         address,
         location: coordinates,
         image: req.file.path,
-        creator
+        creator: req.userData.userId
     });
 
     let user;
 
     try {
-        user = await User.findById(creator);
+        user = await User.findById(req.userData.userId);
     } catch {
         return next(new HttpError('Creating place failed, pleace try again', 500));
     }
